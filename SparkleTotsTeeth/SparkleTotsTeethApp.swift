71aaea1f6fea85c6828241fc384ccf8b681b50b5
Kevin Fredric <joseph_kevin_fredric@s2024.ssts.edu.sg>
2025-11-17 09:01:18 +0800

    Initial Commit

---
+++ SparkleTotsTeeth/SparkleTotsTeethApp.swift
@@ -0,0 +1,17 @@
+//
+//  SparkleTotsTeethApp.swift
+//  SparkleTotsTeeth
+//
+//  Created by Joseph Kevin Fredric on 17/11/25.
+//
+
+import SwiftUI
+
+@main
+struct SparkleTotsTeethApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
